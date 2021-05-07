f2fb9383cebffc5a803bf23686c4f0e1e0743728
kimdhamilton <kimdhamilton@gmail.com>
2021-05-06 19:18:10 -0700

    update repro

--- src/index.ts
+++ src/index.ts
@@ -1,103 +1,91 @@
 #!/usr/bin/env node
-import * as ed25519 from '@transmute/did-key-ed25519';
-import * as vc from '@transmute/vc.js';
+
 import crypto from 'crypto';
 
-// @ts-ignore
-import { Ed25519Signature2018 } from '@transmute/ed25519-signature-2018';
-import { documentLoaderFactory } from '@transmute/jsonld-document-loader';
-
-import DidContext from './did-v1.json';
+const didKeyDriver = require('@digitalbazaar/did-method-key').driver();
 
 
-export const DID_CONTEXT_URL = 'https://www.w3.org/ns/did/v1';
+const didContext = require('did-context');
+const ed25519 = require('ed25519-signature-2020-context');
+const DccContextV1Url = "https://w3id.org/dcc/v1";
+const x25519Ctx = require('x25519-key-agreement-2020-context');
 
-import W3ID_SEC_V1 from './sec-v1.json';
-import W3ID_SEC_V2 from './sec-v2.json';
+import { Ed25519VerificationKey2020 } from '@digitalbazaar/ed25519-verification-key-2020';
+import { Ed25519Signature2020 } from '@digitalbazaar/ed25519-signature-2020';
 
-const W3ID_SEC_URL_V1 = 'https://w3id.org/security/v1';
-const W3ID_SEC_URL_V2 = 'https://w3id.org/security/v2';
+const vc = require('@digitalbazaar/vc');
+import { generateSecureRandom } from 'react-native-securerandom';
+import { contexts as ldContexts, documentLoaderFactory } from '@transmute/jsonld-document-loader';
 
-import W3C_VC_DATA_MODEL_V1 from './vc-v1.json';
-import W3C_VC_DATA_MODEL_EXAMPLES_V1 from './vc-example-v1.json';
-import JWS_V1 from './jws2020.json';
 
-const W3C_VC_DATA_MODEL_URL_V1 = 'https://www.w3.org/2018/credentials/v1';
-const W3C_VC_DATA_MODEL_EXAMPLES_URL_V1 =
-  'https://www.w3.org/2018/credentials/examples/v1';
-const JWS_URL_V1 = 'https://w3id.org/security/jws/v1';
 
-async function generateDidKeyPair(): Promise<ed25519.Ed25519KeyPair> {
 
-  return ed25519.Ed25519KeyPair.generate({
-    secureRandom: () => {
-      return crypto.randomBytes(32);
-    },
-  });
+export function getCustomLoader(): any {
+  const customLoaderProto = documentLoaderFactory.pluginFactory
+    .build({
+      contexts: {
+        ...ldContexts.W3C_Verifiable_Credentials,
+        ...ldContexts.W3ID_Security_Vocabulary,
+        ...ldContexts.W3C_Decentralized_Identifiers
+      },
+    })
+    .addContext({ [ed25519.constants.CONTEXT_URL]: ed25519.contexts.get(ed25519.constants.CONTEXT_URL) })
+    .addContext({ [didContext.constants.DID_CONTEXT_URL]: didContext.contexts.get(didContext.constants.DID_CONTEXT_URL) })
+    .addContext({ [x25519Ctx.constants.CONTEXT_URL]: x25519Ctx.contexts.get(x25519Ctx.constants.CONTEXT_URL) });
+  return customLoaderProto.buildDocumentLoader();
 
 }
+async function generateKey(): Promise<any> {
+ // const BYTES_LENGTH = 32;
+ // const randomBytes = await generateSecureRandom(BYTES_LENGTH);
+  //const {
+  //  didDocument, keyPairs
+  //} = await didKeyDriver.generate(randomBytes);
 
-function createPresentation(holder: string) : any {
+  const {
+    didDocument, keyPairs
+  } = await didKeyDriver.generate();
+
+  const pkey = keyPairs.entries().next().value[1];
+  console.log(JSON.stringify(pkey));
+
+  return pkey;
+}
+
+function createPresentation(holder: string): any {
   return {
     "@context": [
       "https://www.w3.org/2018/credentials/v1",
-      "https://w3id.org/security/jws/v1"
+      "https://w3id.org/security/suites/ed25519-2020/v1"
     ],
     "type": [
       "VerifiablePresentation"
     ],
+    "id": "123",
     "holder": holder
   };
 }
 
-export async function generateDid(): Promise<string> {
-  const keyPair = await generateDidKeyPair();
-  return keyPair.controller;
-}
-
-function generateDidKeySuite(keyPair: ed25519.Ed25519KeyPair): any {
-  const suite = new Ed25519Signature2018({
-   // verificationMethod: keyPair.id,
-    key: keyPair,
-  });
-  return suite;
-}
 
 export async function generateAndProveDid(challenge: string): Promise<any> {
-  console.log('start');
-  const keyPair = await generateDidKeyPair();
-  console.log('keyPair', keyPair);
-  const suite = generateDidKeySuite(keyPair);
-  console.log('suite', suite);
 
-  const documentLoader = documentLoaderFactory.pluginFactory
-    .build()
-    .addContext({ [DID_CONTEXT_URL]: DidContext })
-    .addContext({ [W3ID_SEC_URL_V1]: W3ID_SEC_V1 })
-    .addContext({ [W3ID_SEC_URL_V2]: W3ID_SEC_V2 })
-    .addContext({ [JWS_URL_V1]: JWS_V1 })
-    .addContext({ [W3C_VC_DATA_MODEL_URL_V1]: W3C_VC_DATA_MODEL_V1 })
-    .addContext({
-      [W3C_VC_DATA_MODEL_EXAMPLES_URL_V1]: W3C_VC_DATA_MODEL_EXAMPLES_V1,
-    })
-    .buildDocumentLoader();
-  console.log('documentLoader', documentLoader);
-
-  console.log('controller: ' + keyPair.controller);
-
-  const presentation = createPresentation(keyPair.controller);
-  console.log('presentation', JSON.stringify(presentation, null, 2));
-  //presentation['@context'].push('https://w3id.org/did/v1');
-  const signedPresentation = await vc.ld.signPresentation({
-    presentation: presentation,
-    documentLoader: documentLoader,
-    suite,
-    challenge: challenge,
+  const signingKey = await generateKey();
+  const signatureSuite = new Ed25519Signature2020({
+    key: signingKey,
+    date: '2020-03-10T04:24:12.164Z' // TODO: now
   });
-  console.log('signedPres', signedPresentation);
-  return signedPresentation;
-}
+  const customLoader = getCustomLoader();
 
+  const presentation = createPresentation(signingKey.controller);
+  let result = await vc.signPresentation({
+    presentation: presentation,
+    documentLoader: customLoader,
+    suite: signatureSuite,
+    challenge: challenge
+  });
+  return result;
+
+}
 
 (async () => {
   var text = await generateAndProveDid('3443wrerwrew');
